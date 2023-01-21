9f77a3f7ad242df93150875be8aa7f8667743080
Falko Noé <falnoe@ethz.ch>
2023-01-21 22:29:16 +0000

    Updated with correct paths

--- src/1_qc/02_fastqc.sh
+++ src/1_qc/02_fastqc.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/bash
 
-# Make 
-PROCESSED_FASTQC=results/1_qc/fastqc/
-mkdir -p $PROCESSED_FASTQC
+# Make directories
+PROCESSED_FASTQC=results/1_qc/fastqc
+PROCESSED_MULTIQC=results/1_qc/multiqc
+mkdir -p $PROCESSED_FASTQC $PROCESSED_MULTIQC
 
+# Process with FastQC
 /opt/FastQC/fastqc --extract -o $PROCESSED_FASTQC \
   -t 4 -a data/supplementary-files/adapter_list.txt --kmers 7 \
-  data/tmp/yeast_fastq_full/*fastq.gz \
-  > $PROCESSED_FASTQC/fastqc.out 2> $PROCESSED_FASTQC/fastqc.err
+  data/tmp/yeast_fastq_full/*fastq.gz
+  
+# Combine result with multiqc
+multiqc --outdir $PROCESSED_MULTIQC $PROCESSED_FASTQC
