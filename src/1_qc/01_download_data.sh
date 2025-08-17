117278e23ce83e07759e79c0bf4c3013ccd36aa9
Falko Noé <falnoe@ethz.ch>
2025-08-17 13:21:25 +0000

    Got rid of files and continued adjusting qmd

--- src/1_qc/01_download_data.sh
+++
@@ -1,7 +0,0 @@
-# First we make a directory to house our data
-YEAST_DIR=data/tmp/yeast_fastq_full
-mkdir -p $YEAST_DIR
-cd $YEAST_DIR
-curl -O https://fgcz-gstore.uzh.ch/public/yeast_full.tar
-tar -xvf yeast_full.tar
-rm -f yeast_full.tar
